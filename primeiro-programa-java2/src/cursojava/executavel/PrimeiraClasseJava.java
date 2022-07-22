59150b5fdfe692d0d6fbcb421f0bdc6006ea8f86
elias <elias@air-de-elias>
2022-07-22 09:35:57 -0300

    comitando projeto

---
+++ primeiro-programa-java2/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um método auto executavel em java*/
+	public static void main(String[] args) {
+		
+		/*escreve um texto no huhuh console*/
+		System.out.println("Meu primeiro e programa em java do curso");
+		System.out.println("Java é orientado por objetos");
+
+	}
+
+}
